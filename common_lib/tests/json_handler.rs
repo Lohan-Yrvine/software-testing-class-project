2a726ec6f9f2fd42614516ef149b73a27baf58d7
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2023-11-28 18:50:21 -0300

    refactor(lib tests): propagating errors

--- common_lib/tests/json_handler.rs
+++ common_lib/tests/json_handler.rs
@@ -5,39 +5,36 @@ use anyhow::Result;
 use common_lib::json_handler::JsonHandler;
 
 #[test]
-fn save_file_as_json_test() {
+fn save_file_as_json_test() -> Result<()> {
     let path = "save_file_as_json_test.json";
     let buff = vec![1, 2, 3, 4, 5];
 
-    match JsonHandler::save_as_json(path, &buff) {
-        Ok(_) => {
-            let file = File::open(path).unwrap();
-            let reader = BufReader::new(file);
-            let deserialized: Vec<i32> = serde_json::from_reader(reader).unwrap();
+    JsonHandler::save_as_json(path, &buff)?;
 
-            assert_eq!(deserialized, buff);
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let deserialized: Vec<i32> = serde_json::from_reader(reader)?;
 
-            fs::remove_file(path).unwrap();
-        }
-        Err(_) => assert!(false),
-    }
+    assert_eq!(deserialized, buff);
+
+    fs::remove_file(path)?;
+
+    Ok(())
 }
 
 #[test]
-fn read_json_file_test() {
+fn read_json_file_test() -> Result<()> {
     let path = "read_json_file_test.json";
     let buff = vec![1, 2, 3, 4, 5];
 
-    let serialized = serde_json::to_string_pretty(&buff).unwrap();
-    let mut file = File::create(path).unwrap();
-    file.write_all(serialized.as_bytes()).unwrap();
+    let serialized = serde_json::to_string_pretty(&buff)?;
+    let mut file = File::create(path)?;
+    file.write_all(serialized.as_bytes())?;
 
-    match JsonHandler::read_from_json(path) as Result<Vec<i32>> {
-        Ok(result) => {
-            assert_eq!(result, buff);
+    let result: Vec<u8> = JsonHandler::read_from_json(path)?;
+    assert_eq!(result, buff);
 
-            fs::remove_file(path).unwrap();
-        }
-        Err(_) => assert!(false),
-    }
+    fs::remove_file(path)?;
+
+    Ok(())
 }
