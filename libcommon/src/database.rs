e1733eb836d34355e25b180452cc9bf3975a8a9e
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2023-11-30 20:12:35 -0300

    feat: creating database abstraction

---
+++ libcommon/src/database.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+
+use anyhow::Result;
+
+use crate::json_handler::JsonHandler;
+
+pub struct Database {
+    path: String,
+}
+
+impl Database {
+    pub fn new(path: String) -> Self {
+        Self { path }
+    }
+
+    pub fn insert<T>(&self, value: T) -> Result<()>
+    where
+        T: Into<HashMap<String, String>>,
+    {
+        let mut db_content: Vec<HashMap<String, String>> = JsonHandler::read_from_json(&self.path)?;
+        db_content.push(value.into());
+
+        JsonHandler::save_as_json(&self.path, &db_content)
+    }
+
+    pub fn query<T>(&self, key: &str) -> Result<Option<T>>
+    where
+        T: From<HashMap<String, String>>,
+    {
+        let db_content: Vec<HashMap<String, String>> = JsonHandler::read_from_json(&self.path)?;
+
+        if let Some(hashmap) = db_content
+            .into_iter()
+            .find(|hashmap| hashmap.contains_key(key))
+        {
+            return Ok(Some(hashmap.into()));
+        }
+
+        Ok(None)
+    }
+
+    pub fn update<T>(&self, key: &str, element: T) -> Result<Option<()>>
+    where
+        T: Into<HashMap<String, String>>,
+    {
+        let mut db_content: Vec<HashMap<String, String>> = JsonHandler::read_from_json(&self.path)?;
+
+        if let Some(hashmap) = db_content
+            .iter_mut()
+            .find(|hashmap| hashmap.contains_key(key))
+        {
+            *hashmap = element.into();
+
+            JsonHandler::save_as_json(&self.path, &db_content)?;
+
+            return Ok(Some(()));
+        }
+
+        Ok(None)
+    }
+
+    pub fn delete<T>(&self, key: &str) -> Result<Option<T>>
+    where
+        T: From<HashMap<String, String>>,
+    {
+        let mut db_content: Vec<HashMap<String, String>> = JsonHandler::read_from_json(&self.path)?;
+
+        if let Some(idx) = db_content
+            .iter()
+            .position(|hashmap| hashmap.contains_key(key))
+        {
+            let removed = db_content.swap_remove(idx);
+
+            JsonHandler::save_as_json(&self.path, &db_content)?;
+
+            return Ok(Some(removed.into()));
+        }
+
+        Ok(None)
+    }
+}
--- libcommon/src/lib.rs
+++ libcommon/src/lib.rs
@@ -6,4 +6,5 @@ mod io_toolkit {
 pub use io_toolkit::io_handler;
 pub use io_toolkit::json_handler;
 
+pub mod database;
 pub mod priority_queue;
