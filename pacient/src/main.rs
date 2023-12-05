62f3660ec9d11240df92959d08256866d4a130a5
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2023-12-05 11:17:02 -0300

    config: moving anyhow to workspace depedency

--- pacient/src/main.rs
+++ pacient/src/main.rs
@@ -1,24 +1,22 @@
 use std::env;
 
+use anyhow::Result;
 use common::io_handler::{DefaultIOHandler, IOHandler};
 use common::priority_queue::PriorityQueue;
 use dotenv::dotenv;
 
 use pacient::pacient_manager::PacientManager;
 
-fn main() {
+fn main() -> Result<()> {
     dotenv().ok();
 
-    let queue_file_path =
-        env::var("PACIENT_QUEUE_FILE_PATH").expect("Unable to get enviroment variable");
+    let queue_file_path = env::var("PACIENT_QUEUE_FILE_PATH")?;
 
     let io_handler = IOHandler::default();
-    io_handler
-        .set_remove_file_on_exit_handler(
-            queue_file_path.clone(),
-            Some("\n\nPrograma encerrado.".to_string()),
-        )
-        .expect("Unable to set remove on exit handler");
+    io_handler.set_remove_file_on_exit_handler(
+        queue_file_path.clone(),
+        Some("\n\nPrograma encerrado.".to_string()),
+    )?;
 
     let mut manager = PacientManager::new(io_handler, PriorityQueue::new(), queue_file_path);
     manager.start();
